92a6b8c603dce0d63fa5021be8af4759d7de179c
kumarraja2k05 <kumarraja2k05@gmail.com>
2022-12-09 11:00:20 +0530

    implemented modals and routing with sign-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomeComponent } from './components/home/home.component';
+import { MessageModalComponent } from './components/message-modal/message-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent,
+    MessageModalComponent
   ],
   imports: [
     BrowserModule
