b87d8c1745bf3b2532ba9df5df6e20a36108075c
kumarraja2k05 <kumarraja2k05@gmail.com>
2022-12-19 14:17:28 +0530

    added drive data table for get post request

--- src/app/components/drive-management/hiring-drive-form/hiring-drive-form.component.ts
+++ src/app/components/drive-management/hiring-drive-form/hiring-drive-form.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { DriveDates } from 'src/app/models/drive-dates';
+import { HringDriveService } from 'src/app/services/hring-drive.service';
+import {Subject} from 'rxjs';
+import {DataTableDirective} from 'angular-datatables';
+
 declare var window: any;
 
 @Component({
@@ -8,24 +12,21 @@ declare var window: any;
   styleUrls: ['./hiring-drive-form.component.css']
 })
 export class HiringDriveFormComponent {
-  panelistformModal: any;
   drive = new DriveDates();
   driveDates:any=[];
-  constructor() {}
+  driveformModal: any;
+  Drivedata: any;
+  dtOptions:DataTables.Settings={};
+  dtTrigger: Subject<any> = new Subject<any>();
+
+  constructor(private driveService:HringDriveService) {
+    this.driveService.hiring_drives().subscribe((result)=>{
+      this.Drivedata =result;
+    })
+  }
 
   ngOnInit(): void {
     this.driveDates.push(this.drive);
-    this.panelistformModal = new window.bootstrap.Modal(
-      document.getElementById('HiringDriveModal')
-    );
-  }
-  
-  openHirirngDriveFormModal() {
-    this.panelistformModal.show();
-  }
-  saveDrive() {
-    // confirm or save something
-    this.panelistformModal.hide();
   }
 
   addDates()
@@ -41,6 +42,10 @@ export class HiringDriveFormComponent {
 
   onSubmitDriveData(data:any)
   {
-    console.log(data);
+    this.driveService.saveDriveData(data).subscribe((result)=>{
+      console.warn(result);
+      this.Drivedata=result;
+      // this.dtTrigger.next();
+    })
   }
 }
--- src/app/services/hring-drive.service.ts
+++ src/app/services/hring-drive.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpErrorResponse,HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +11,33 @@ export class HringDriveService {
   url = "https://69i2ptm1f4.execute-api.us-east-1.amazonaws.com/dev/drive";
   constructor(private http: HttpClient) { }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
+
   hiring_drives()
   {
-    return this.http.get(this.url);
+    return this.http.get(this.url)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
   
   saveDriveData(data:any)
   {
-    return this.http.post(this.url,data);
+    return this.http.post(this.url,data).pipe(
+      catchError(this.handleError)
+    );
   }
 }
