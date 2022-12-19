cbd52256cb4a42e0fb3a5c268f814e0f02c80e08
shankargupta1179 <shankarguptha9676@gmail.com>
2022-12-19 11:21:00 +0530

    Add dates dynamically on clicking the add date button

--- src/app/components/drive-management/hiring-drive-form/hiring-drive-form.component.ts
+++ src/app/components/drive-management/hiring-drive-form/hiring-drive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { DriveDates } from 'src/app/models/drive-dates';
 declare var window: any;
 
 @Component({
@@ -9,10 +9,12 @@ declare var window: any;
 })
 export class HiringDriveFormComponent {
   panelistformModal: any;
- 
+  drive = new DriveDates();
+  driveDates:any=[];
   constructor() {}
 
   ngOnInit(): void {
+    this.driveDates.push(this.drive);
     this.panelistformModal = new window.bootstrap.Modal(
       document.getElementById('HiringDriveModal')
     );
@@ -24,5 +26,16 @@ export class HiringDriveFormComponent {
   saveDrive() {
     // confirm or save something
     this.panelistformModal.hide();
+  }
+
+  addDates()
+  {
+    this.drive= new DriveDates();
+    this.driveDates.push(this.drive);
+  }
+
+  removeDates(index:any)
+  {
+    this.driveDates.splice(index,1);
   }
 }
---
+++ src/app/models/drive-dates.ts
@@ -0,0 +1,8 @@
+import { Time } from "@angular/common";
+
+export class DriveDates
+{
+    roundName:any;
+    roundDate: any;
+    roundTime:any;
+}
