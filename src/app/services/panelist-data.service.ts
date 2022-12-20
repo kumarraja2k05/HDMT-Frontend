2eaf5a80042c14ed15dd3d37aa102b8a54d8a6d6
shankargupta1179 <shankarguptha9676@gmail.com>
2022-12-20 23:47:40 +0530

    Secured the get api of panelist with jwt

--- src/app/services/panelist-data.service.ts
+++ src/app/services/panelist-data.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
+import { TokenServiceService } from './token-service.service';
 @Injectable({
   providedIn: 'root'
 })
 export class PanelistDataService {
   url = "https://69i2ptm1f4.execute-api.us-east-1.amazonaws.com/dev/panelist";
-  constructor(private http: HttpClient) { }
+  token = this.tokenService.getToken();
+  public readonly header = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${this.token}`
+  });
+  public readonly requestOptions={headers: this.header};
+  constructor(private http: HttpClient,private tokenService:TokenServiceService) { }
 
   panelists()
   {
-    return this.http.get(this.url);
+    return this.http.get(this.url,this.requestOptions);
   }
   
   savePanelistData(data:any)
